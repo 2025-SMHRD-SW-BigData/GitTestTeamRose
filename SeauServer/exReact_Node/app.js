fc3ea240dea6647af0e7eab5f72db184d8a0f044
PJJ <qkrwnwls@gmail.com>
2025-06-21 14:59:09 +0900

    server추가

---
+++ SeauServer/exReact_Node/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// node에서 react로 응답 보내기 위해 cors설정
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+//http://localhost:3001
+app.post('/', (req, res)=>{
+    const {id, pw, nick} = req.body
+    console.log(req.body);
+    console.log('접근 확인!')
+    //console.log('회원가입 요청:', id, pw, nick)
+    if(id!='asd'){
+        res.send('회원가입 성공')
+    } else {
+        res.send('회원가입 실패')
+    }
+})
+
+app.post('/login', (req,res) => {
+    const {id, pw} = req.body;
+    console.log('로그인 요청')
+    console.log(req.body);
+    if(id=='rose'&&pw=='1234'){
+        res.send('로그인 성공')
+    } else {
+        res.send('로그인 실패')
+    }
+})
+app.listen(3001)
