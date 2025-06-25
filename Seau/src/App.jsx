3a39d3b860cc66686e161f802f2156938c8344a5
JKU <smfqh13@gmail.com>
2025-06-25 10:42:51 +0900

    ui

--- Seau/src/App.jsx
+++ Seau/src/App.jsx
@@ -8,23 +8,42 @@ import Mypage from './components/Mypage'
 import Home1 from './components/Home1'
 import { UserContext } from './context/UserContext'
 import Weather from './components/Weather'
+import { Navigation } from "./components/Navigation"
+import { AuthSidebar } from "./components/AuthSiderbar"
+import './App.css'
 
 
 function App() {
   const [isOauth, setIsOauth] = useState(false);
   const [userId, setUserId] = useState('');
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarMode, setSidebarMode] = useState("login")
+
+  const openSidebar = (mode) => {
+    setSidebarMode(mode)
+    console.log(mode)
+    setSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setSidebarOpen(false)
+  }
 
   return (
-    <UserContext.Provider value = {{isOauth, setIsOauth, userId, setUserId}}>
+    <UserContext.Provider value={{ isOauth, setIsOauth, userId, setUserId }}>
+      {/* 네비게이션 */}
+      <Navigation onLoginClick={() => openSidebar("login")} onSignupClick={() => openSidebar("signup")} />
       <Routes>
         <Route path='/weather' element={<Weather></Weather>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/home1' element={<Home1></Home1>}></Route>
         <Route path='/join' element={<Join></Join>}></Route>
         <Route path='/join2' element={<Join2></Join2>}></Route>
-        <Route path='/' element={<Login></Login>}></Route>
+        <Route path='/login' element={<Login onLoginClick={openSidebar}></Login>}></Route>
         <Route path='/mypage' element={<Mypage ></Mypage>}></Route>
+        <Route path='/' element={<AuthSidebar isOpen={sidebarOpen} onClose={closeSidebar} initialMode={sidebarMode} />}></Route>
       </Routes>
+
     </UserContext.Provider>
   )
 }
